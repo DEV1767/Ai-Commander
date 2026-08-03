566dcc3aa1ef27228b1cedf0b074dce988fa810a
shivam chaudhary <shivamchy076@gmail.com>
2026-08-03 19:39:07 +0530

    Added doc name in tools

--- src/incident_analyzer/nodes.py
+++ src/incident_analyzer/nodes.py
@@ -9,6 +9,10 @@ from .state import ErrorExtractor, ErrorState, Risk, Suggestion
 
 
 def extractor_node(state: ErrorState) -> dict:
+    """
+     Extract the logs,error,description  from the raw_text.
+    """
+    
     structured_llm = model.with_structured_output(ErrorExtractor)
     response = structured_llm.invoke(build_extractor_prompt(state))
 
@@ -21,6 +25,10 @@ def extractor_node(state: ErrorState) -> dict:
 
 
 def risk_analysis_node(state: ErrorState) -> dict:
+    """
+     Analyze the log,errors and on that basis return the risk of error in the term of high ,low and medium .
+    """
+    
     structured_llm = model.with_structured_output(Risk)
     response = structured_llm.invoke(build_risk_prompt(state))
 
@@ -28,6 +36,10 @@ def risk_analysis_node(state: ErrorState) -> dict:
 
 
 def error_explanation_node(state: ErrorState) -> dict:
+    """
+    Explain the error on the basis of logs,error and risk level. 
+    """
+    
     structured_llm = model2.with_structured_output(Suggestion)
     response = structured_llm.invoke(build_error_explanation_prompt(state))
 
@@ -35,12 +47,19 @@ def error_explanation_node(state: ErrorState) -> dict:
 
 
 def error_prevention_node(state: ErrorState) -> dict:
+    """
+    Suggest the user how to prevent the error in future .
+    """
     structured_llm = model2.with_structured_output(Suggestion)
     response = structured_llm.invoke(build_error_prevention_prompt(state))
 
     return {"prevention": response.prevention}
 
 def route_by_risk(state:ErrorState)->str:
+    """
+    Check the risk of error and then help to explain the error.
+    """
+    
     if state['risk']=="Low":
        return "skip"
      
